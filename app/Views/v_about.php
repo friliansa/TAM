2804a8277b1659895109d52eddc87bda112036db
friliansa <friliansa@gmail.com>
2021-09-16 22:02:41 +0700

    web

---
+++ app/Views/v_about.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="text-center">
+        <img src="https://drive.google.com/uc?export=view&id=1S-AbdSgk5Kdcxdg8gBqHEd2QwDw-ZzCf" alt="logo" width="200" class="img-thumbnail rounded">
+        <h1 class="display-4">Praktikum Web Lanjut Dengan Studi Kasus Blog App</h1>
+        <p class="lead">Programming | Codeigniter 4 | Php</p>
+    </div>
+</div>
